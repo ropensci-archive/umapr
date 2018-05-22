2055269b04e258bce219211c7430315bd611585e
Sean Hughes <seaaan@gmail.com>
2018-05-22 15:45:08 -0700

    fix simple broken tests

--- tests/testthat/test-umapr.R
+++ tests/testthat/test-umapr.R
@@ -26,28 +26,28 @@ test_that("Things work as expected", {
   data = cbind(matrix(rexp( 100 * 10, runif(1, 1E-5, 1E-3) ), 100, 10))
 
   # The function should check the input types to make sure they are correct
-  expect_error(umap(data = "Not a matrix"), "data must be a matrix")
-  expect_error(umap(data = "This is not a matrix or a data frame"), "data must be a matrix or a data.frame")
-  expect_error(umap(data = data, n_neighbors = "Not numeric"), "n_neighbors must be numeric")
-  expect_error(umap(data = data, n_components = 2.4), "n_components must be an int")
+  expect_error(umap(data = "Not a matrix"), "Data must be a data frame or a matrix")
+  expect_error(umap(data = "This is not a matrix or a data frame"), "Data must be a data frame or a matrix.")
+  expect_error(umap(data = data, n_neighbors = "Not numeric"), "n_neighbors is not a number")
+  expect_error(umap(data = data, n_components = "Not numeric"), "n_components is not a number")
   # metric must be one of the options listed here: https://github.com/lmcinnes/umap/blob/bf1c3e5c89ea393c9de10bd66c5e3d9bc30588ee/umap/umap_.py#L1211
-  expect_error(umap(data = data, metric = "not a valid metric"), "metric must be valid")
-  expect_error(umap(data = data, n_epochs = 2.4), "n_epochs must be an int (not actually sure about this one)")
-  expect_error(umap(data = data, alpha = "Not numeric"), "alpha must be numeric")
+  expect_error(umap(data = data, metric = "not a valid metric"), "Valid metrics are")
+  expect_error(umap(data = data, n_epochs = "Not numeric"), "n_epochs is not a number")
+  expect_error(umap(data = data, alpha = "Not numeric"), "alpha is not a numeric")
   expect_error(umap(data = data, init = "not a valid init"), "init must be one of 'spectral', 'random', or a numpy array of initial embedding positions")
-  expect_error(umap(data = data, spread = "Not numeric"), "spread must be numeric")
-  expect_error(umap(data = data, min_dist = "Not numeric"), "min_dist must be numeric")
-  expect_error(umap(data = data, set_op_mix_ratio = "Not numeric"), "set_op_mix_ratio must be numeric")
-  expect_error(umap(data = data, local_connectivity = 2.4), "local_connectivity must be an int")
-  expect_error(umap(data = data, bandwidth = "Not numeric"), "bandwidth must be numeric")
-  expect_error(umap(data = data, gamma = "Not numeric"), "gamma must be numeric")
-  expect_error(umap(data = data, negative_sample_rate = 2.4), "negative_sample_rate must be an int")
-  expect_error(umap(data = data, a = "Not numeric"), "a must be numeric")
-  expect_error(umap(data = data, b = "Not numeric"), "b must be numeric")
-  expect_error(umap(data = data, random_state = 2.4), "random_state must be an int")
+  expect_error(umap(data = data, spread = "Not numeric"), "spread is not a numeric")
+  expect_error(umap(data = data, min_dist = "Not numeric"), "min_dist is not a numeric")
+  expect_error(umap(data = data, set_op_mix_ratio = "Not numeric"), "set_op_mix_ratio is not a numeric")
+  expect_error(umap(data = data, local_connectivity = 2.4), "local_connectivity is not a an int")
+  expect_error(umap(data = data, bandwidth = "Not numeric"), "bandwidth is not a numeric")
+  expect_error(umap(data = data, gamma = "Not numeric"), "gamma is not a numeric")
+  expect_error(umap(data = data, negative_sample_rate = 2.4), "negative_sample_rate is not a an int")
+  expect_error(umap(data = data, a = "Not numeric"), "a is not a numeric")
+  expect_error(umap(data = data, b = "Not numeric"), "b is not a numeric")
+  expect_error(umap(data = data, random_state = 2.4), "random_state is not a an int")
   expect_error(umap(data = data, metric_kwds = 2.4), "metric_kwds must be a Python dictionary object, you can create it using 'reticulate::py_dict()'")
-  expect_error(umap(data = data, angular_rp_forest = 2.4), "angular_rp_forest must be a boolean")
-  expect_error(umap(data = data, verbose = 2.4), "verbose must be a boolean")
+  expect_error(umap(data = data, angular_rp_forest = 2.4), "angular_rp_forest is not a flag")
+  expect_error(umap(data = data, verbose = 2.4), "verbose is not a flag")
 
   # try running umap with the same seed twice, see if you get the same thing
   expect_true(identical(umap(data = data, random_state = 3L),
@@ -56,15 +56,15 @@ test_that("Things work as expected", {
 
 # test_that("R6 tests",
 #           {
-#               
+#
 #               set.seed(1)
 #               data = cbind(matrix(rexp( 100 * 10, runif(1, 1E-5, 1E-3) ), 100, 10))
 #               colnames(data) <- c(letters[1:10])
 #               out <- umap(data)
-#               
+#
 #               expect_equal(class(out)[1], "umap_obj")
 #               pl <- out$plot("a")
 #               expect_equal(class(pl)[-1], "ggplot")
-#             
-#             
+#
+#
 #           })
