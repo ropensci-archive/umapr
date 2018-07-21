cf7f34f9b63d2c852afd99fb5d46c643bd30016d
Sean Hughes <seaaan@gmail.com>
2018-07-21 03:04:48 -0700

    Handle renamed parameter alpha vs initial_alpha. Closes #25.

--- R/umap.R
+++ R/umap.R
@@ -2,82 +2,82 @@
 #'
 #' @description Provides an interface to the UMAP algorithm implemented in Python.
 #'
-#' @references Leland McInnes and John Healy (2018). UMAP: Uniform Manifold 
-#' Approximation and Projection for Dimension Reduction. 
+#' @references Leland McInnes and John Healy (2018). UMAP: Uniform Manifold
+#' Approximation and Projection for Dimension Reduction.
 #' ArXiv e-prints 1802.03426.
 #'
 #' @param data data frame or matrix. input data.
-#' @param n_neighbors integer. The size of local neighborhood 
-#' (in terms of number of neighboring sample points) used for manifold 
-#' approximation. Larger values result in more global views of the manifold, 
-#' while smaller values result in more local data being preserved. In general 
+#' @param n_neighbors integer. The size of local neighborhood
+#' (in terms of number of neighboring sample points) used for manifold
+#' approximation. Larger values result in more global views of the manifold,
+#' while smaller values result in more local data being preserved. In general
 #' values should be in the range 2 to 100.
-#' @param n_components integer The dimension of the space to embed into. This 
-#' defaults to 2 to provide easy visualization, but can reasonably be set to 
+#' @param n_components integer The dimension of the space to embed into. This
+#' defaults to 2 to provide easy visualization, but can reasonably be set to
 #' any integer value in the range 2 to 100.
-#' @param metric character. The metric to use to compute distances in high 
-#' dimensional space. If a string is passed it must match a valid predefined 
-#' metric. If a general metric is required a function that takes two 1d arrays 
-#' and returns a float can be provided. For performance purposes it is required 
-#' that this be a numba jit'd function. Valid string metrics include: euclidean, 
-#' manhattan, chebyshev, minkowski, canberra, braycurtis, mahalanobis, 
-#' wminkowski, seuclidean, cosine, correlation, haversine, hamming, jaccard, 
-#' dice, russelrao, kulsinski, rogerstanimoto, sokalmichener, sokalsneath, yule. 
-#' Metrics that take arguments (such as minkowski, mahalanobis etc.) can have 
-#' arguments passed via the metric_kwds dictionary. At this time care must be 
-#' taken and dictionary elements must be ordered appropriately; this will 
+#' @param metric character. The metric to use to compute distances in high
+#' dimensional space. If a string is passed it must match a valid predefined
+#' metric. If a general metric is required a function that takes two 1d arrays
+#' and returns a float can be provided. For performance purposes it is required
+#' that this be a numba jit'd function. Valid string metrics include: euclidean,
+#' manhattan, chebyshev, minkowski, canberra, braycurtis, mahalanobis,
+#' wminkowski, seuclidean, cosine, correlation, haversine, hamming, jaccard,
+#' dice, russelrao, kulsinski, rogerstanimoto, sokalmichener, sokalsneath, yule.
+#' Metrics that take arguments (such as minkowski, mahalanobis etc.) can have
+#' arguments passed via the metric_kwds dictionary. At this time care must be
+#' taken and dictionary elements must be ordered appropriately; this will
 #' hopefully be fixed in the future.
 #' @param n_epochs integer The number of training epochs to use in optimization.
 #' @param alpha numeric. The initial learning rate for the embedding optimization.
-#' @param init character. How to initialize the low dimensional embedding. 
+#' @param init character. How to initialize the low dimensional embedding.
 #' Options are: 'spectral' (use a spectral embedding of the fuzzy 1-skeleton),
-#' 'random' (assign initial embedding positions at random), 
+#' 'random' (assign initial embedding positions at random),
 #' * A numpy array of initial embedding positions.
-#' @param spread numeric. The effective scale of embedded points. 
-#' In combination with ``min_dist`` this determines how clustered/clumped the 
+#' @param spread numeric. The effective scale of embedded points.
+#' In combination with ``min_dist`` this determines how clustered/clumped the
 #' embedded points are.
-#' @param min_dist numeric.  The effective minimum distance between embedded 
-#' points. Smaller values will result in a more clustered/clumped embedding 
-#' where nearby points on the manifold are drawn closer together, while larger 
-#' values will result on a more even dispersal of points. The value should be 
-#' set relative to the ``spread`` value, which determines the scale at which 
+#' @param min_dist numeric.  The effective minimum distance between embedded
+#' points. Smaller values will result in a more clustered/clumped embedding
+#' where nearby points on the manifold are drawn closer together, while larger
+#' values will result on a more even dispersal of points. The value should be
+#' set relative to the ``spread`` value, which determines the scale at which
 #' embedded points will be spread out.
-#' @param set_op_mix_ratio numeric. Interpolate between (fuzzy) union and 
-#' intersection as the set operation used to combine local fuzzy simplicial 
-#' sets to obtain a global fuzzy simplicial sets. Both fuzzy set operations use 
-#' the product t-norm. The value of this parameter should be between 0.0 and 
-#' 1.0; a value of 1.0 will use a pure fuzzy union, while 0.0 will use a pure 
+#' @param set_op_mix_ratio numeric. Interpolate between (fuzzy) union and
+#' intersection as the set operation used to combine local fuzzy simplicial
+#' sets to obtain a global fuzzy simplicial sets. Both fuzzy set operations use
+#' the product t-norm. The value of this parameter should be between 0.0 and
+#' 1.0; a value of 1.0 will use a pure fuzzy union, while 0.0 will use a pure
 #' fuzzy intersection.
-#' @param local_connectivity integer The local connectivity required -- i.e. 
-#' the number of nearest neighbors that should be assumed to be connected at a 
-#' local level. The higher this value the more connected the manifold becomes 
-#' locally. In practice, this should be not more than the local intrinsic 
+#' @param local_connectivity integer The local connectivity required -- i.e.
+#' the number of nearest neighbors that should be assumed to be connected at a
+#' local level. The higher this value the more connected the manifold becomes
+#' locally. In practice, this should be not more than the local intrinsic
 #' dimension of the manifold.
-#' @param bandwidth numeric. The effective bandwidth of the kernel if we view 
-#' the algorithm as similar to Laplacian eigenmaps. Larger values induce more 
-#' connectivity and a more global view of the data, smaller values concentrate 
+#' @param bandwidth numeric. The effective bandwidth of the kernel if we view
+#' the algorithm as similar to Laplacian eigenmaps. Larger values induce more
+#' connectivity and a more global view of the data, smaller values concentrate
 #' more locally.
-#' @param gamma numeric. Weighting applied to negative samples in low 
-#' dimensional embedding optimization. Values higher than one will result in 
+#' @param gamma numeric. Weighting applied to negative samples in low
+#' dimensional embedding optimization. Values higher than one will result in
 #' greater weight being given to negative samples.
-#' @param negative_sample_rate numeric. The number of negative edge/1-simplex 
-#' samples to use per positive edge/1-simplex sample in optimizing the low 
+#' @param negative_sample_rate numeric. The number of negative edge/1-simplex
+#' samples to use per positive edge/1-simplex sample in optimizing the low
 #' dimensional embedding.
-#' @param a numeric. More specific parameters controlling the embedding. 
-#' If NULL, these values are set automatically as determined by ``min_dist`` 
+#' @param a numeric. More specific parameters controlling the embedding.
+#' If NULL, these values are set automatically as determined by ``min_dist``
 #' and ``spread``.
-#' @param b numeric. More specific parameters controlling the embedding. 
-#' If NULL, these values are set automatically as determined by ``min_dist`` 
+#' @param b numeric. More specific parameters controlling the embedding.
+#' If NULL, these values are set automatically as determined by ``min_dist``
 #' and ``spread``.
-#' @param random_state integer. If integer, random_state is the seed used by the 
-#' random number generator; If NULL, the random number generator is the 
+#' @param random_state integer. If integer, random_state is the seed used by the
+#' random number generator; If NULL, the random number generator is the
 #' RandomState instance used by `np.random`.
-#' @param metric_kwds reticulate dictionary. Arguments to pass on to the metric, 
+#' @param metric_kwds reticulate dictionary. Arguments to pass on to the metric,
 #' such as the ``p`` value for Minkowski distance.
-#' @param angular_rp_forest logical. Whether to use an angular random projection 
-#' forest to initialise the approximate nearest neighbor search. This can be 
-#' faster, but is mostly on useful for metric that use an angular style distance 
-#' such as cosine, correlation etc. In the case of those metrics angular forests 
+#' @param angular_rp_forest logical. Whether to use an angular random projection
+#' forest to initialise the approximate nearest neighbor search. This can be
+#' faster, but is mostly on useful for metric that use an angular style distance
+#' such as cosine, correlation etc. In the case of those metrics angular forests
 #' will be chosen automatically.
 #' @param verbose logical. Controls verbosity of logging.
 #'
@@ -90,6 +90,7 @@
 #' umap(as.matrix(iris[, 1:4]))
 #' umap(iris[, 1:4])
 umap <- function(data,
+                 include_input = TRUE,
                  n_neighbors = 15L,
                  n_components = 2L,
                  metric = "euclidean",
@@ -131,32 +132,67 @@ umap <- function(data,
   assert_that(is.flag(angular_rp_forest))
   assert_that(is.flag(verbose))
 
-  umap_vec <- umap_module$UMAP(
-    n_neighbors = as.integer(n_neighbors),
-    n_components = as.integer(n_components),
-    metric = metric,
-    n_epochs = n_epochs,
-    alpha = alpha,
-    init = init,
-    spread = spread,
-    min_dist = min_dist,
-    set_op_mix_ratio = set_op_mix_ratio,
-    local_connectivity = local_connectivity,
-    bandwidth = bandwidth,
-    gamma = gamma,
-    negative_sample_rate = as.integer(negative_sample_rate),
-    a = a,
-    b = a,
-    random_state = random_state,
-    metric_kwds = metric_kwds,
-    angular_rp_forest = angular_rp_forest,
-    verbose = verbose
-  )$fit_transform(as.matrix(data))
-  
+  # keyword "alpha" was renamed "initial_alpha" in a later version of the
+  # python library, try running it both ways in case of failure
+  umap_vec <- tryCatch(
+    umap_module$UMAP(
+      n_neighbors = as.integer(n_neighbors),
+      n_components = as.integer(n_components),
+      metric = metric,
+      n_epochs = n_epochs,
+      initial_alpha = alpha,
+      init = init,
+      spread = spread,
+      min_dist = min_dist,
+      set_op_mix_ratio = set_op_mix_ratio,
+      local_connectivity = local_connectivity,
+      bandwidth = bandwidth,
+      gamma = gamma,
+      negative_sample_rate = as.integer(negative_sample_rate),
+      a = a,
+      b = a,
+      random_state = random_state,
+      metric_kwds = metric_kwds,
+      angular_rp_forest = angular_rp_forest,
+      verbose = verbose
+      )$fit_transform(as.matrix(data)),
+    error = function(e) {
+      if (grepl("initial_alpha", e$message)) {
+        umap_module$UMAP(
+          n_neighbors = as.integer(n_neighbors),
+          n_components = as.integer(n_components),
+          metric = metric,
+          n_epochs = n_epochs,
+          alpha = alpha,
+          init = init,
+          spread = spread,
+          min_dist = min_dist,
+          set_op_mix_ratio = set_op_mix_ratio,
+          local_connectivity = local_connectivity,
+          bandwidth = bandwidth,
+          gamma = gamma,
+          negative_sample_rate = as.integer(negative_sample_rate),
+          a = a,
+          b = a,
+          random_state = random_state,
+          metric_kwds = metric_kwds,
+          angular_rp_forest = angular_rp_forest,
+          verbose = verbose
+        )$fit_transform(as.matrix(data))
+      } else {
+        throw(e)
+      }
+    }
+  )
   colnames(umap_vec) <- paste0("UMAP", seq_len(ncol(umap_vec)))
-  
-  output <- data.frame(cbind(data, umap_vec))
-  
+
+  # attach input data to UMAP embeddings if desired
+  if (include_input) {
+    output <- data.frame(cbind(data, umap_vec))
+  } else {
+    output <- data.frame(umap_vec)
+  }
+
   #make_umap_object(output)
   output
 }
