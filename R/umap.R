c2cf79ba74d277092cfa8dc88d659f3b31e51a83
TomKellyGenetics <tomkellygenetics@gmail.com>
2019-07-20 15:14:04 +0900

    update inputs for latest version of mpython module

--- R/umap.R
+++ R/umap.R
@@ -95,20 +95,28 @@ umap <- function(data,
                  n_components = 2L,
                  metric = "euclidean",
                  n_epochs = NULL,
+                 learning_rate = 1.0,
                  alpha = 1.0,
                  init = "spectral",
                  spread = 1.0,
                  min_dist = 0.1,
                  set_op_mix_ratio = 1.0,
                  local_connectivity = 1L,
+                 repulsion_strength = 1.0,
                  bandwidth = 1.0,
                  gamma = 1.0,
                  negative_sample_rate = 5L,
+                 transform_queue_size = 4.0,
                  a = NULL,
                  b = NULL,
                  random_state = NULL,
                  metric_kwds = dict(),
                  angular_rp_forest = FALSE,
+                 target_n_neighbors = -1L,
+                 target_metric = "categorical",
+                 target_metric_kwds = dict(),
+                 target_weight =  0.5,
+                 transform_seed = 42L,
                  verbose = FALSE) {
   assert_that(is.matrix(data) | is.data.frame(data), msg = "Data must be a data frame or a matrix.")
   if (!all(unlist(lapply(data, is.numeric)))) stop("All columns should be numeric.")
@@ -116,46 +124,59 @@ umap <- function(data,
   assert_that(is.count(n_components))
   assert_that(is.character(metric), msg = "Valid string metrics include: euclidean, manhattan, chebyshev, minkowski, canberra, braycurtis, mahalanobis, wminkowski, seuclidean, cosine, correlation, haversine, hamming, jaccard, dice, russelrao, kulsinski, rogerstanimoto, sokalmichener, sokalsneath, yule.")
   assert_that(is.null(n_epochs) | is.count(n_epochs), msg = "n_epochs is not a count (a single positive integer)")
+  assert_that(is.numeric(learning_rate))
   assert_that(is.numeric(alpha))
   assert_that(init %in% c("spectral", "random"), msg = "init must be one of 'spectral', 'random', or a numpy array of initial embedding positions")
   assert_that(is.numeric(spread))
   assert_that(is.numeric(min_dist))
   assert_that(is.numeric(set_op_mix_ratio))
   assert_that(is.count(local_connectivity))
+  assert_that(is.numeric(repulsion_strength))
   assert_that(is.numeric(bandwidth))
   assert_that(is.numeric(gamma))
   assert_that(is.count(negative_sample_rate))
+  assert_that(is.numeric(transform_queue_size))
   assert_that(is.null(a) | is.numeric(a))
   assert_that(is.null(b) | is.numeric(b))
   assert_that(is.null(random_state) | is.count(random_state))
   assert_that(is_dict(metric_kwds), msg = "metric_kwds must be a Python dictionary object, you can create it using 'reticulate::dict()'")
   assert_that(is.flag(angular_rp_forest))
+  assert_that(is.integer(target_n_neighbors))
+  assert_that(is.character(target_metric) | is.function(target_metric))
+  assert_that(is_dict(target_metric_kwds))
+  assert_that(is.numeric(target_weight))
+  assert_that(is.integer(transform_seed))
   assert_that(is.flag(verbose))
-
+  
   # keyword "alpha" was renamed "initial_alpha" in a later version of the
   # python library, try running it both ways in case of failure
   umap_vec <- tryCatch(
     umap_module$UMAP(
-      n_neighbors = as.integer(n_neighbors),
-      n_components = as.integer(n_components),
-      metric = metric,
-      n_epochs = n_epochs,
-      initial_alpha = alpha,
-      init = init,
-      spread = spread,
-      min_dist = min_dist,
-      set_op_mix_ratio = set_op_mix_ratio,
-      local_connectivity = local_connectivity,
-      bandwidth = bandwidth,
-      gamma = gamma,
-      negative_sample_rate = as.integer(negative_sample_rate),
-      a = a,
-      b = a,
-      random_state = random_state,
+      n_neighbors = r_to_py(as.integer(n_neighbors)),
+      n_components = r_to_py(as.integer(n_components)),
+      metric = r_to_py(metric),
+      n_epochs = r_to_py(n_epochs),
+      learning_rate = r_to_py(as.numeric(learning_rate)),
+      init = r_to_py(init),
+      min_dist = r_to_py(as.numeric(min_dist)),
+      spread = r_to_py(as.numeric(spread)),
+      set_op_mix_ratio = r_to_py(as.numeric(set_op_mix_ratio)),
+      local_connectivity = r_to_py(as.integer(local_connectivity)),
+      repulsion_strength = r_to_py(as.numeric(repulsion_strength)),
+      negative_sample_rate = r_to_py(as.integer(negative_sample_rate)),
+      transform_queue_size = r_to_py(as.numeric(transform_queue_size)),
+      a = r_to_py(a),
+      b = r_to_py(b),
+      random_state = r_to_py(random_state),
       metric_kwds = metric_kwds,
-      angular_rp_forest = angular_rp_forest,
-      verbose = verbose
-      )$fit_transform(as.matrix(data)),
+      angular_rp_forest = r_to_py(angular_rp_forest),
+      target_n_neighbors = as.integer(target_n_neighbors),
+      target_metric = r_to_py(target_metric),
+      target_metric_kwds = target_metric_kwds,
+      target_weight =  r_to_py(target_weight),
+      transform_seed = r_to_py(as.integer(transform_seed)),
+      verbose = r_to_py(verbose)
+    )$fit_transform(r_to_py(as.matrix(data))),
     error = function(e) {
       if (grepl("initial_alpha", e$message)) {
         umap_module$UMAP(
@@ -173,7 +194,7 @@ umap <- function(data,
           gamma = gamma,
           negative_sample_rate = as.integer(negative_sample_rate),
           a = a,
-          b = a,
+          b = b,
           random_state = random_state,
           metric_kwds = metric_kwds,
           angular_rp_forest = angular_rp_forest,
@@ -185,14 +206,14 @@ umap <- function(data,
     }
   )
   colnames(umap_vec) <- paste0("UMAP", seq_len(ncol(umap_vec)))
-
+  
   # attach input data to UMAP embeddings if desired
   if (include_input) {
     output <- data.frame(cbind(data, umap_vec))
   } else {
     output <- data.frame(umap_vec)
   }
-
+  
   #make_umap_object(output)
   output
 }
