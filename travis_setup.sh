cee6d4a58a6051f9f74161d3a7a97c36ad753156
TomKellyGenetics <tomkellygenetics@gmail.com>
2019-07-22 10:26:23 +0900

    configure travis

--- travis_setup.sh
+++ travis_setup.sh
@@ -2,9 +2,9 @@
 
 # install python
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
-wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
 fi
 
 bash miniconda.sh -b -p $HOME/miniconda
